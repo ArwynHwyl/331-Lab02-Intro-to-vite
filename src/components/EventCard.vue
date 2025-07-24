f88369b5450e21626ffd23691188b62102dc2668
Arwynhwyl <arwynhwyl@Arwynhwyls-MacBook-Air.local>
2025-07-25 04:15:00 +0700

    1.9

--- src/components/EventCard.vue
+++ src/components/EventCard.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
-import { ref } from 'vue'
 import type { Event } from '@/types'
+import { RouterLink } from 'vue-router'
 defineProps<{
   event: Event
 }>()
@@ -18,23 +18,27 @@ defineProps<{
 </script>
 
 <template>
-  <div class="event-class">
+  <RouterLink class="event-link" :to="{ name: 'event-detail-view', params: { id: event.id } }">
     <div class="event-card">
       <h2>{{ event.title }}</h2>
       <span>@{{ event.time }} on {{ event.date }}</span>
     </div>
-  </div>
+  </RouterLink>
 </template>
 <style scoped>
-.event-class {
+.event-card {
   padding: 20px;
   width: 250px;
   cursor: pointer;
   border: 1px solid #39495c;
   margin-bottom: 18px;
 }
-.event-class:hover {
+.event-card:hover {
   transform: scale(1.01);
   box-shadow: 0 3px 12px 0 rgb(0, 0, 0.2);
+}
+.event-link {
+  text-decoration: none;
+  color: #2c3e50;
 }
 </style>
