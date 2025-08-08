a6f08d94ff3f63aac70bcb1ebcce9181fed77cf8
Arwynhwyl <arwynhwyl@Arwynhwyls-MacBook-Air.local>
2025-08-08 09:40:28 +0700

    Lab 5 2.3

--- src/views/EventListView.vue
+++ src/views/EventListView.vue
@@ -38,7 +38,7 @@ onMounted(() => {
 <template>
   <h1>Event For Good</h1>
   <!-- new element-->
-  <div class="events">
+  <div class="flex flex-col items-center">
     <EventCard v-for="event in events" :key="event.id" :event="event" />
   </div>
   <RouterLink
@@ -60,11 +60,6 @@ onMounted(() => {
 </template>
 
 <style scoped>
-.events {
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-}
 .pagination {
   display: flex;
   width: 290px;
