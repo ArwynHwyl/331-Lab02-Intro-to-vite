208f36994d2bb5ba28c55c1587f0180c45e634b6
Arwynhwyl <arwynhwyl@Arwynhwyls-MacBook-Air.local>
2025-08-08 10:30:08 +0700

    Lab05 13.7

--- src/App.vue
+++ src/App.vue
@@ -8,6 +8,7 @@ const { messages } = storeToRefs(store)
 </script>
 
 <template>
+  <SpeedInsights />
   <div class="text-center font-sans text-gray-700 antialiased min-h-screen bg-white">
     <header>
       <div id="flashMessage" class="animate-fade" v-if="messages">
